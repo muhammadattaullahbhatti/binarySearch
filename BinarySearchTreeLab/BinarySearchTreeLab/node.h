274e3c102a382011b107d66effd2bb854b561d68
mrmuhammadazeemrao <56134289+mrmuhammadazeemrao@users.noreply.github.com>
2019-10-18 21:05:54 +0500

    Update node.h

--- BinarySearchTreeLab/BinarySearchTreeLab/node.h
+++ BinarySearchTreeLab/BinarySearchTreeLab/node.h
@@ -7,9 +7,12 @@ class Node
 {
 public:
 	int data;
+	// made both below variables on the same line to avoid no of lines and to make less complexity
 	Node * left;
 	Node * right;
+	//default constructor
 	Node();
+	//parametrized constructor
 	Node(int);
 
 };
